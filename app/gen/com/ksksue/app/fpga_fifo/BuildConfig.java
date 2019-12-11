dbeb2e596642dcd17b9a79d0cca03e8259fefdab
benlohman <benw.lohman@gmail.com>
2019-12-10 20:08:54 -0700

    2st

---
+++ app/gen/com/ksksue/app/fpga_fifo/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+package com.ksksue.app.fpga_fifo;
+
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
+public final class BuildConfig {
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
+}
